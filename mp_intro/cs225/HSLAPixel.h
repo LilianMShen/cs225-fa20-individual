567ab99d8baffe1bc4e28c4bb74a727225dd735d
Lilian Shen <lmshen2@linux-a2.ews.illinois.edu>
2020-09-03 14:54:46 -0500

    Finished the lab

--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -6,5 +6,16 @@
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
+
 namespace cs225 {
+    class HSLAPixel{
+        public:
+        double h, s, l, a;
+        HSLAPixel();
+        HSLAPixel(double hue, double saturation, double luminance);
+        HSLAPixel(double hue, double saturation, double luminance, double alpha);
+    };
 }
+
