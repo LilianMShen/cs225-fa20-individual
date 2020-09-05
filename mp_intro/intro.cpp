2d9bd9a7475d66a23ebd1aa37eef907021f58d53
Lilian Shen <lmshen2@linux-a2.ews.illinois.edu>
2020-09-05 16:28:32 -0500

    Forgot how to commit, just finished part 2 and working on part 3 currently. Have a background color up

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -2,15 +2,51 @@
 #include "cs225/HSLAPixel.h"
 
 #include <string>
+#include <cmath>
 
+using cs225::HSLAPixel;
+using cs225::PNG;
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  PNG* image = new PNG();
+  image->readFromFile(inputFile);
+  unsigned width = image->width();
+  unsigned height = image->height();
+  PNG* output = new PNG(width, height);
+
+  unsigned newX = 0;
+  unsigned newY = 0;
+
+  for (unsigned int x = 0; x < width; x++) {
+    for (unsigned int y = 0; y < height; y++) {
+      HSLAPixel& oldPixel = image->getPixel(x,y);
+      newX = width - x - 1;
+      newY = height - y - 1;
+      output->getPixel(newX, newY) = oldPixel;
+    }
+  }
+  output->writeToFile(outputFile);
 }
 
-cs225::PNG myArt(unsigned int width, unsigned int height) {
-  cs225::PNG png(width, height);
-  // TODO: Part 3
+PNG myArt(unsigned int width, unsigned int height) {
+  PNG png(width, height);
+  HSLAPixel* background = new HSLAPixel(260.0, 1.0, 0.0);
+  HSLAPixel* primaryColor = new HSLAPixel(333.0, 0.65, 0.82);
+  HSLAPixel* secondaryColor = new HSLAPixel(253.0, 0.67, 0.77);
+
+  unsigned halfHeight = height / 2;
+
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      HSLAPixel& currPixel = png.getPixel(x,y);
+      currPixel = *background;
+    }
+  }
+  /**
+  for (unsigned i = 0; i < width; i++) {
+    unsigned j = sin(i);
+    HSLAPixel& currPixel = png.getPixel(x,y);
+  }*/
 
   return png;
 }
