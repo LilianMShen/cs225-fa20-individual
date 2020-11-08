80c31dbfd49f79c315b08f75334fd610db4b6103
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2020-11-08 10:13:23 -0600

    finished the lab

--- lab_heaps/heap.cpp
+++ lab_heaps/heap.cpp
@@ -8,35 +8,40 @@ template <class T, class Compare>
 size_t heap<T, Compare>::root() const
 {
     // @TODO Update to return the index you are choosing to be your root.
-    return 0;
+
+    // i want my root to be index 1
+    return 1;
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::leftChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the left child.
-    return 0;
+    return (2 * currentIdx);
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::rightChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the right child.
-    return 0;
+    return ((2 * currentIdx) + 1);
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::parent(size_t currentIdx) const
 {
     // @TODO Update to return the index of the parent.
-    return 0;
+    return (currentIdx / 2);
 }
 
 template <class T, class Compare>
 bool heap<T, Compare>::hasAChild(size_t currentIdx) const
 {
     // @TODO Update to return whether the given node has a child
-    return false;
+    if (_elems.size() <= (currentIdx * 2)) {
+        return false;
+    }
+    return true;
 }
 
 template <class T, class Compare>
@@ -44,13 +49,35 @@ size_t heap<T, Compare>::maxPriorityChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the child with highest priority
     ///   as defined by higherPriority()
-    return 0;
+    size_t left, right;
+    left = leftChild(currentIdx);
+    right = rightChild(currentIdx);
+
+    if (_elems.size() <= right) {
+        return left;
+    }
+
+    if (higherPriority(_elems[left], _elems[right])) {
+        return left;
+    }
+
+    return right;
 }
 
 template <class T, class Compare>
 void heap<T, Compare>::heapifyDown(size_t currentIdx)
 {
     // @TODO Implement the heapifyDown algorithm.
+    if (!hasAChild(currentIdx)) {
+        return;
+    }
+
+    size_t max = maxPriorityChild(currentIdx);
+
+    if (higherPriority(_elems[max], _elems[currentIdx])) {
+        std::swap(_elems[max], _elems[currentIdx]);
+        heapifyDown(max);
+    }
 }
 
 template <class T, class Compare>
@@ -70,32 +97,51 @@ heap<T, Compare>::heap()
 {
     // @TODO Depending on your implementation, this function may or may
     ///   not need modifying
+    _elems.push_back(T());
 }
 
 template <class T, class Compare>
 heap<T, Compare>::heap(const std::vector<T>& elems) 
 {
     // @TODO Construct a heap using the buildHeap algorithm
+    _elems.push_back(T());
+
+    _elems.insert(_elems.end(), elems.begin(), elems.end());
+
+    for (size_t i = parent(elems.size()); i > 0; i--) {
+        heapifyDown(i);
+    }
 }
 
 template <class T, class Compare>
 T heap<T, Compare>::pop()
 {
     // @TODO Remove, and return, the element with highest priority
-    return T();
+    T temp = _elems[root()];
+
+    std::swap(_elems[root()], _elems[_elems.size() - 1]);
+
+    _elems.pop_back();
+
+    heapifyDown(root());
+
+    return temp;
 }
 
 template <class T, class Compare>
 T heap<T, Compare>::peek() const
 {
     // @TODO Return, but do not remove, the element with highest priority
-    return T();
+    return _elems[root()];
 }
 
 template <class T, class Compare>
 void heap<T, Compare>::push(const T& elem)
 {
     // @TODO Add elem to the heap
+    _elems.push_back(elem);
+
+    heapifyUp(_elems.size() - 1);
 }
 
 template <class T, class Compare>
@@ -103,6 +149,11 @@ void heap<T, Compare>::updateElem(const size_t & idx, const T& elem)
 {
     // @TODO In-place updates the value stored in the heap array at idx
     // Corrects the heap to remain as a valid heap even after update
+
+    _elems[idx] = elem;
+
+    heapifyUp(idx);
+    heapifyDown(idx);
 }
 
 
@@ -110,7 +161,10 @@ template <class T, class Compare>
 bool heap<T, Compare>::empty() const
 {
     // @TODO Determine if the heap is empty
-    return true;
+    if (_elems.size() <= 1) {
+        return true;
+    }
+    return false;
 }
 
 template <class T, class Compare>
